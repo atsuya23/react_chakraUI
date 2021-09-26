a7579428f5dddafc709204c8e7a0306bfbfb1950
atsuya23 <90808047+atsuya23@users.noreply.github.com>
2021-09-26 17:40:23 +0900

    試しボタン

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <ChakraProvider>
+      <Button colorScheme="teal">ボタン</Button>
+    </ChakraProvider>
   );
 }
