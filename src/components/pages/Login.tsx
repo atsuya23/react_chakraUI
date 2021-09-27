9c1ccfb569ba0019c4babae58ab8f59838dd2357
atsuya23 <90808047+atsuya23@users.noreply.github.com>
2021-09-27 18:29:36 +0900

    PrimaryButtonのコンポーネント化

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,13 +1,7 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack
-} from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
   return (
@@ -19,9 +13,7 @@ export const Login: VFC = memo(() => {
         <Divider my={3} />
         <Stack spacing={4} py={4} px={9}>
           <Input placeholder="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-            ログイン
-          </Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
