92baa9e870f71e25e61e1fecd988f0319e7c3da5
atsuya23 <90808047+atsuya23@users.noreply.github.com>
2021-09-28 10:50:51 +0900

    ログインユーザ情報の取得（保持）

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,10 +4,12 @@ import { useHistory } from "react-router-dom";
 
 import { useMessage } from "./useMessage";
 import { User } from "../types/api/user";
+import { useLoginUser } from "../hooks/useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
+  const { setLoginUser } = useLoginUser();
 
   const [loading, setLoading] = useState(false);
 
@@ -18,21 +20,23 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
+            setLoading(false);
             showMessage({
               title: "ユーザーが見つかりません",
               status: "error"
             });
           }
         })
-        .catch(() =>
-          showMessage({ title: "ログインできません", status: "error" })
-        )
-        .finally(() => setLoading(false));
+        .catch(() => {
+          showMessage({ title: "ログインできません", status: "error" });
+          setLoading(false);
+        });
     },
-    [history, showMessage]
+    [history, showMessage, setLoginUser]
   );
   return { login, loading };
 };
